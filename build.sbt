5a09729338a261748909b124de2f5808a416988f
maxticorn <letunovskeymax@gmail.com>
2021-04-11 22:59:26 +0500

    effects

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "test"
 version := "0.1"
 
 scalaVersion := "2.13.5"
+
+libraryDependencies += "dev.zio" %% "zio" % "1.0.5"
--- src/main/scala/Third.scala
+++ src/main/scala/Third.scala
@@ -1,6 +1,16 @@
-import scala.io.StdIn
+import zio.{ExitCode, URIO}
+import zio.console.{getStrLn, putStrLn}
 
-object Third extends App {
-  val emailRegex = "^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$".r
-  println(if (emailRegex matches StdIn.readLine()) "valid" else "invalid")
+object Third extends zio.App {
+  private val emailRegex = "^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$".r
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    (for {
+      input <- getStrLn
+      _ <- putStrLn(if (emailRegex matches input) "valid" else "invalid")
+    } yield ())
+      .fold(
+        _ => ExitCode.failure,
+        _ => ExitCode.success
+      )
 }
